4d33a4382cc55c9fcf0fd5136b4152f349e22d46
warlyware <danward@gmail.com>
2015-07-31 21:08:37 -0700

    Add angular-router, gulp

--- public/javascripts/index.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module('gameTime', []);
-
-
-
- $(".button-collapse").sideNav();
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
