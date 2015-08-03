775a4cbe70c0f19d2236a266cb4d677e66930add
warlyware <danward@gmail.com>
2015-08-03 11:02:56 -0700

    Closes #2: Add authorization

--- src/javascripts/controllers/auth.js
+++ src/javascripts/controllers/auth.js
@@ -1,4 +1,34 @@
 angular.module('GameTime')
 .controller('AuthCtrl', function($scope, $state) {
   console.log('AuthCtrl loaded.');
+  var ref = new Firebase("https://dw-game-time.firebaseio.com");
+
+  $scope.registerUser = function() {
+    console.log('registerUser')
+    ref.createUser({
+      email    : $scope.user.email,
+      password : $scope.user.password
+    }, function(error, userData) {
+      if (error) {
+        console.log("Error creating user:", error);
+      } else {
+        console.log("Successfully created user account with uid:", userData.uid);
+      }
+    });
+  }
+
+  $scope.loginUser = function() {
+    ref.authWithPassword({
+      email    : $scope.user.email,
+      password : $scope.user.password
+    }, function(error, authData) {
+      if (error) {
+        console.log("Login Failed!", error);
+      } else {
+        console.log("Authenticated successfully with payload:", authData);
+      }
+    });
+  }
+
+
 });
